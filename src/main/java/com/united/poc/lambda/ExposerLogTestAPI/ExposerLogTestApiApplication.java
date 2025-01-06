8068b15df8ff679b3975d59c17e30191705ca04b
Camilo Contreras <Camilo.Contreras@united.com>
2025-01-06 13:31:49 -0300

    initial commit

---
+++ src/main/java/com/united/poc/lambda/ExposerLogTestAPI/ExposerLogTestApiApplication.java
@@ -0,0 +1,13 @@
+package com.united.poc.lambda.ExposerLogTestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExposerLogTestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExposerLogTestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/united/poc/lambda/ExposerLogTestAPI/ExposerLogTestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.united.poc.lambda.ExposerLogTestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExposerLogTestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
