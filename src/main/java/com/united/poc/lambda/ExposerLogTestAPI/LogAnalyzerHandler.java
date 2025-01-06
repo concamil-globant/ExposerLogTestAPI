cc648e7c605369c53e427880b10ccb3333329252
Camilo Contreras <Camilo.Contreras@united.com>
2025-01-06 14:01:54 -0300

    working on

--- src/main/java/com/united/poc/lambda/ExposerLogTestAPI/LogAnalyzerHandler.java
+++ src/main/java/com/united/poc/lambda/ExposerLogTestAPI/LogAnalyzerHandler.java
@@ -1,4 +1,6 @@
 package com.united.poc.lambda.ExposerLogTestAPI;
 
-public class LogAnalyzerHandler {
+import org.springframework.cloud.function.adapter.aws.FunctionInvoker;
+
+public class LogAnalyzerHandler extends FunctionInvoker {
 }
